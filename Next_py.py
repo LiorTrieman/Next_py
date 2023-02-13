3b17809146f57d3952be95dbc3f3f946e04d89eb
LiorTrieman <45787086+LiorTrieman@users.noreply.github.com>
2023-02-13 10:31:07 +0200

    Update Next_py.py
    updated with hidden methods and default arguments

--- Next_py.py
+++ Next_py.py
@@ -1,24 +1,41 @@
 # creating a class of our favorite animal
-
+# updating the code:
 class Tiger:
 
-    def __init__(self):
+    def __init__(self, name="no_name"):
         self.color = "brown"
-        self.age = 0
+        self._age = 0
+        self._name = name
+       # count_animals += 1
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
 
     def birthday_1(self):
-        self.age += 1
+        self._age += 1
 
     def get_age(self):
-        return self.age
+        return self._age
 
 
 def __main__():
-    Mirons_Tiger = Tiger()
+    # count_animals = 0  # numer of animals from this class
+    Mirons_Tiger = Tiger("Miron")
     Shoham_Tiger = Tiger()
+    print(Mirons_Tiger.get_name())
+    print(Shoham_Tiger.get_name())
+    Mirons_Tiger.set_name("Miron_2")
+    Shoham_Tiger.set_name("Shoham")
+    print(Mirons_Tiger.get_name())
+    print(Shoham_Tiger.get_name())
+
     Mirons_Tiger.birthday_1()
     Mirons_Tiger.birthday_1()
     print(Mirons_Tiger.get_age())
+    #print(count_animals)
 
 
 __main__()
