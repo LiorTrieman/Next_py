f2445c5d53a700b48323ec2983102cc47382bbe9
LiorTrieman <45787086+LiorTrieman@users.noreply.github.com>
2023-02-13 11:31:09 +0200

    Update Next_py.py
    adding a Pixel class

--- Next_py.py
+++ Next_py.py
@@ -1,5 +1,6 @@
 # creating a class of our favorite animal
 # updating the code:
+# adding a class that creats a pixel
 class Tiger:
 
     def __init__(self, name="no_name"):
@@ -21,23 +22,68 @@ class Tiger:
         return self._age
 
 
-def __main__():
+"""
+_x - x coordinate
+_y - y coordinate
+_red - a value between 0 and 255
+_green - a value between 0 and 255
+_blue - a value between 0 and 255
+"""
+
+
+class Pixel:
+
+    def __init__(self, x=0, y=0, red=0, green=0, blue=0):
+        self._x = x
+        self._y = y
+        self._red = red
+        self._green = green
+        self._blue = blue
+
+    def set_coords(self, x, y):
+        self._x = x
+        self._y = y
+
+    def set_grayscale(self):
+        mean_color = (self._red + self._green + self._blue)/3
+        self._red = int(mean_color)
+        self._green = int(mean_color)
+        self._blue = int(mean_color)
+
+    def print_pixel_info(self):
+        add_color = "0"
+        print(add_color)
+        if self._red == 0 & self._blue == 0 & self._green > 49:
+            add_color = "Green"
+        if self._red == 0 & self._green == 0 & self._blue > 49:
+            add_color = "Blue"
+        if self._blue == 0 & self._green == 0 & self._red > 49:
+            add_color = "Red"
+        print(f"X:{self._x}, Y:{self._y} , Color:{self._red, self._green, self._blue}")
+
+
+def main():
+    pixel = Pixel(5, 6, 255)
+    pixel.print_pixel_info()
+    pixel.set_grayscale()
+    pixel.print_pixel_info()
+
     # count_animals = 0  # numer of animals from this class
-    Mirons_Tiger = Tiger("Miron")
-    Shoham_Tiger = Tiger()
-    print(Mirons_Tiger.get_name())
-    print(Shoham_Tiger.get_name())
-    Mirons_Tiger.set_name("Miron_2")
-    Shoham_Tiger.set_name("Shoham")
-    print(Mirons_Tiger.get_name())
-    print(Shoham_Tiger.get_name())
+    miron_tiger = Tiger("Miron")
+    shoham_tiger = Tiger()
+    print(miron_tiger.get_name())
+    print(shoham_tiger.get_name())
+    miron_tiger.set_name("Miron_2")
+    shoham_tiger.set_name("Shoham")
+    print(miron_tiger.get_name())
+    print(shoham_tiger.get_name())
 
-    Mirons_Tiger.birthday_1()
-    Mirons_Tiger.birthday_1()
-    print(Mirons_Tiger.get_age())
-    #print(count_animals)
+    miron_tiger.birthday_1()
+    miron_tiger.birthday_1()
+    print(miron_tiger.get_age())
 
 
-__main__()
+main()
+
 
 
